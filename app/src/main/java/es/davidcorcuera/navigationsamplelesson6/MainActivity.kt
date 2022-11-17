de8dd77e215f147185dfcc654a50369873235e50
David Corcuera <david.corcuera@gmail.com>
2022-11-17 20:35:15 +0100

    add Up Button with NavigationUI

--- app/src/main/java/es/davidcorcuera/navigationsamplelesson6/MainActivity.kt
+++ app/src/main/java/es/davidcorcuera/navigationsamplelesson6/MainActivity.kt
@@ -2,6 +2,9 @@ package es.davidcorcuera.navigationsamplelesson6
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import es.davidcorcuera.navigationsamplelesson6.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -9,7 +12,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         setSupportActionBar(binding.toolbar)
+
+        // get NavController
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        // get appBarConfiguration
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        // assign NavigationUI to toolbar
+        binding.toolbar
+            .setupWithNavController(navController, appBarConfiguration)
+
     }
+
 }
