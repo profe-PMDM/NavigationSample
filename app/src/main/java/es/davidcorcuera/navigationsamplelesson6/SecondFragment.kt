309863d4e19fca45d611b516dad78ca9538571d5
David Corcuera <david.corcuera@gmail.com>
2022-11-17 18:38:16 +0100

    add a button to navigate from Second Fragment to Third Fragment

--- app/src/main/java/es/davidcorcuera/navigationsamplelesson6/SecondFragment.kt
+++ app/src/main/java/es/davidcorcuera/navigationsamplelesson6/SecondFragment.kt
@@ -6,18 +6,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import es.davidcorcuera.navigationsamplelesson6.databinding.FragmentSecondBinding
 
 class SecondFragment : Fragment() {
+
+    private lateinit var binding: FragmentSecondBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
-        val binding = DataBindingUtil.inflate<FragmentSecondBinding>(inflater,
+        binding = DataBindingUtil.inflate(inflater,
             R.layout.fragment_second,
             container,
             false)
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val btn = binding.button
+        btn.setOnClickListener {
+            this.findNavController().navigate(R.id.action_secondFragment_to_thirdFragment)
+        }
+
+    }
+
 }
